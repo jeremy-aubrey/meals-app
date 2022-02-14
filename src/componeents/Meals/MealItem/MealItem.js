5ee3dba1997d8c03af232c75f6018be061cad40e
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-14 12:00:42 -0600

    added meal id to MealItemForm props

--- src/componeents/Meals/MealItem/MealItem.js
+++ src/componeents/Meals/MealItem/MealItem.js
@@ -19,7 +19,7 @@ const MealItem = props => {
                 </div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm id={props.id}/>
             </div>
         </li>
     )
