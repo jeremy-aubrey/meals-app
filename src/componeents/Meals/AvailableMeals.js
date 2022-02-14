3e8cf58dfbda93c79b4eff6987d9f5d4197d7f59
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-14 11:19:13 -0600

    wrapped meals ul in Card

--- src/componeents/Meals/AvailableMeals.js
+++ src/componeents/Meals/AvailableMeals.js
@@ -1,49 +1,52 @@
 import React from "react";
+import Card from "../UI/Card";
 
 import styles from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
     {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
     },
     {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
     },
     {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
     },
     {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
     },
-  ];
+];
 
 const AvailableMeals = () => {
 
     const mealsList = DUMMY_MEALS.map(meal => {
         return (
-        <li>
-            {meal.name}
-        </li>
+            <li>
+                {meal.name}
+            </li>
         );
     });
 
     return (
         <section className={styles.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
