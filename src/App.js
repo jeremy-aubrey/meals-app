04f7c6f561facadd6b2d2dbc6db69f7fddd3ada8
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-14 11:10:37 -0600

    imported Meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./componeents/Layout/Header";
+import Meals from "./componeents/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
