823aa73b037e51f0f2c55e778d03586ea51bcc4a
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-15 10:42:13 -0600

    consumes context

--- src/componeents/Layout/HeaderCartButton.js
+++ src/componeents/Layout/HeaderCartButton.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => {
+
+    const cartCtx = useContext(CartContext);
+
+    const cartItemCount = cartCtx.items.reduce((curNum, item) => {
+        return curNum + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
@@ -11,7 +19,7 @@ const HeaderCartButton = props => {
             </span>
             <span>Your Cart</span>
             <span className={styles.badge}>
-                3
+                {cartItemCount}
             </span>
         </button>
     );
