2755b1e2f4a39def067b2c6f6beb97a72506d88c
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-15 09:58:59 -0600

    added cart state; added show / hide cart handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./componeents/Layout/Header";
 import Meals from "./componeents/Meals/Meals";
 import Cart from "./componeents/Cart/Cart";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
