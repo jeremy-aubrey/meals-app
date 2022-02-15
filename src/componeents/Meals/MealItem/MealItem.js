2c71aa2ae776740d7113889e3cf9bca1126c5756
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-15 12:11:40 -0600

    calls addItem -> addToCartHandler through context

--- src/componeents/Meals/MealItem/MealItem.js
+++ src/componeents/Meals/MealItem/MealItem.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 import styles from "./MealItem.module.css";
 
 const MealItem = props => {
 
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem(
+            {
+                id: props.id,
+                name: props.name,
+                amount: amount,
+                price: props.price
+            }
+        )
+    }
 
     return (
         <li className={styles.meal}>
@@ -19,7 +33,7 @@ const MealItem = props => {
                 </div>
             </div>
             <div>
-                <MealItemForm id={props.id}/>
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
             </div>
         </li>
     )
