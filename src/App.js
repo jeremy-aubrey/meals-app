271d6771f1132bd29642dd542dfc55dea2c9af1d
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-15 09:52:03 -0600

    added Cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "./componeents/Layout/Header";
 import Meals from "./componeents/Meals/Meals";
+import Cart from "./componeents/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
