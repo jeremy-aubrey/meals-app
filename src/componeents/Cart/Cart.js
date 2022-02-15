60d548dca95360b466818031e8a33855b10ac8ae
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-15 10:12:00 -0600

    forwarded onCloseCart to Modal

--- src/componeents/Cart/Cart.js
+++ src/componeents/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = props => {
         })
 
     return (
-        <Modal>
+        <Modal onCloseCart={props.onCloseCart}>
             <ul className={styles['cart-items']}>
                 {cartItems}
             </ul>
