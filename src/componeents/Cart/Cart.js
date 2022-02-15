2a229bb92789a774f26f426a142ca25d0579a164
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-15 09:49:18 -0600

    wrapped Cart in Modal component

--- src/componeents/Cart/Cart.js
+++ src/componeents/Cart/Cart.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Modal from "../UI/Modal";
 
 import styles from "./Cart.module.css";
 
 const Cart = props => {
 
-    const cartItems = [{id: c1, name: 'Sushi', amount: 2, price: 12.99}]
-    .map(item => {
-        return <li>{item.name}</li>
-    })
+    const cartItems = [{ id: c1, name: 'Sushi', amount: 2, price: 12.99 }]
+        .map(item => {
+            return <li>{item.name}</li>
+        })
 
     return (
-        <div>
+        <Modal>
             <ul className={styles['cart-items']}>
-            {cartItems}
+                {cartItems}
             </ul>
             <div className={styles.total}>
                 <span>Total Amount</span>
@@ -22,8 +23,8 @@ const Cart = props => {
                 <button className={styles['button--alt']}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
-        </div>
-    )
+        </Modal>
+    );
 }
 
 export default Cart;
