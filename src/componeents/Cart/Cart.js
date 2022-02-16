b89cfa8b48fa374ce634f5cf79acd4e46080afd0
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-16 09:37:49 -0600

    updated Cart to display CartItems

--- src/componeents/Cart/Cart.js
+++ src/componeents/Cart/Cart.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 
 import styles from "./Cart.module.css";
+import { isCompositeComponent } from "react-dom/cjs/react-dom-test-utils.production.min";
 
 const Cart = props => {
 
-    const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
-        .map(item => {
-            return <li>{item.name}</li>
-        })
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => {};
+
+    const cartItemAddHandler = item => {};
+
+    const cartItems = (
+        <ul className={styles['cart-items']}>
+            {cartCtx.items.map((item) => (
+                <CartItem 
+                key={item.id} 
+                name={item.name} 
+                amount={item.amount} 
+                price={item.price}
+                onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
+        </ul>
+    )
 
     return (
         <Modal onCloseCart={props.onCloseCart}>
-            <ul className={styles['cart-items']}>
                 {cartItems}
-            </ul>
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button className={styles['button--alt']} onClick={props.onCloseCart}>Close</button>
-                <button className={styles.button}>Order</button>
+                {hasItems && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     );
