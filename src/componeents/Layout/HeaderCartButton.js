8cc37c489bb16f520d535cfa03235cc6f8a5f81e
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-02-16 10:36:25 -0600

    added button animation when items are added / removed

--- src/componeents/Layout/HeaderCartButton.js
+++ src/componeents/Layout/HeaderCartButton.js
@@ -1,19 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
 
-    const cartItemCount = cartCtx.items.reduce((curNum, item) => {
+    const cartItemCount = items.reduce((curNum, item) => {
         return curNum + item.amount;
     }, 0);
 
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+    
+
+    useEffect(() => {
+
+        if(cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        //reset button state so animation can replay
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300)
+
+        //clean up
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
